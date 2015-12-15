df2984295d4a414a2f9f531d85594fd8d0f64bd6
Steven Havko <stevenhavko@gmail.com>
2015-12-15 16:07:23 -0500

    Added Player class

--- game.rb
+++ game.rb
@@ -1,4 +1,9 @@
+require '.\Player'
+player = Player.new
+player.name = "Steven"
 while (0 == 0)
-  puts "There is only darkness" 
+  puts "There is only darkness"
+  puts "Hello " + player.name 
   response = gets 	
+
 end
